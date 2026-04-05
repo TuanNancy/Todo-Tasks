1765efde3f2f95c7097a28fb3f86a287be91f06a
VNATdev-ops <vuanhtuan18092004@gmail.com>
2026-04-05 20:45:52 +0700

    Update authControllers.js

--- backend/src/controllers/authControllers.js
+++ backend/src/controllers/authControllers.js
@@ -97,7 +97,12 @@ export const login = async (req, res) => {
 };
 
 export const logout = async (req, res) => {
-  res.clearCookie("token", { path: "/" });
+  res.clearCookie("token", {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    path: "/",
+  });
   res.status(200).json({ message: "Đăng xuất thành công" });
 };
 
